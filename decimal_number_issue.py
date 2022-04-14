40b622f6c5b1e7ac7826f9536acbf276903d169e
Ian Washburn <iwashburn99@gmail.com>
2022-04-14 18:04:43 -0400

    decimal number can now move around screen

--- decimal_number_issue.py
+++ decimal_number_issue.py
@@ -14,11 +14,12 @@ class Test(Scene):
 
         brace = always_redraw(Brace, line, UP)
 
-        distance = DecimalNumber(0, num_decimal_places=2, font_size=24)
+        distance = DecimalNumber(0, num_decimal_places=2, font_size=48)
 
-        always(distance.next_to, line)
+        always(distance.shift, np.array((0., 0.005, 0.)))
+        #always(brace.next_to, line, UP)
         f_always(distance.set_value, lambda: line.get_length() / SCALE)
-        f_always(brace.rotate, line.get_angle)
+        #f_always(brace.rotate, line.get_angle)
 
         self.add(distance)
         #self.add(brace)
