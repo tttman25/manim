81c09c4c5d7ae290ca57db9193ffc36417a167cc
Ian Washburn <iwashburn99@gmail.com>
2022-04-13 23:01:06 -0400

    attempting to find bug

--- decimal_number_issue.py
+++ decimal_number_issue.py
@@ -4,25 +4,24 @@ from manimlib import *
 class Test(Scene):
     def construct(self):
         SCALE = 3
-        plane = NumberPlane((-2, 2),(-2, 2)).scale(SCALE)
+        plane = NumberPlane((-2, 2), (-2, 2)).scale(SCALE)
         plane.set_stroke(BLUE_E, 1)
 
         self.add(plane)
 
-        p1 = Point(plane.c2p(1,0))
+        p1 = Point(plane.c2p(1, 0))
         line = Line(plane.get_origin(), p1.get_location()).set_stroke(GREEN_E, 2)
 
         brace = always_redraw(Brace, line, UP)
 
         distance = DecimalNumber(0, num_decimal_places=2, font_size=24)
-        distance.arrange(RIGHT)
 
-        always(distance.next_to, line, UP)
-        f_always(distance.set_value, lambda : line.get_length() / SCALE)
+        always(distance.next_to, line)
+        f_always(distance.set_value, lambda: line.get_length() / SCALE)
         f_always(brace.rotate, line.get_angle)
 
         self.add(distance)
-        self.add(brace)
+        #self.add(brace)
 
         self.play(
             ShowCreation(line)
